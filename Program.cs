96b910c58e43ad5063dd34a0827955ae76182429
Alisson Jardel <alissonjark401@outlook.com>
2019-12-26 13:31:40 -0300

    Removeed unnecessary uses

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
 ﻿using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenQA.Selenium;
-using NUnit.Framework;
 
 namespace SeleniumTest2
 {
